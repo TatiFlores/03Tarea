9a82952673d40d2673f94b5a21ac51faa9945b1e
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-10-08 17:31:17 -0300

    Se agregan iteraciones y plots

--- p1.py
+++ p1.py
@@ -32,3 +32,46 @@ def rk3_step(y_n, v_n, h, f):
     y_n1 = y_n + (k1[0] + 4 * k2[0] + k3[0])/6
     v_n1 = v_n + (k1[1] + 4 * k2[1] + k3[1])/6
     return y_n1, v_n1
+
+N_steps = 40000
+h = 20*np.pi / N_steps
+y_caso1 = np.zeros(N_steps)
+v_caso1 = np.zeros(N_steps)
+
+y_caso1[0] = 0.1
+v_caso1[0] = 0
+for i in range(1, N_steps):
+    y_caso1[i], v_caso1[i] = rk3_step(y_caso1[i-1], v_caso1[i-1], h, f)
+
+y_caso2 = np.zeros(N_steps)
+v_caso2 = np.zeros(N_steps)
+
+y_caso2[0] = 4
+v_caso2[0] = 0
+for i in range(1, N_steps):
+    y_caso2[i], v_caso2[i] = rk3_step(y_caso2[i-1], v_caso2[i-1], h, f)
+
+s_rk = [h * i for i in range(N_steps)]
+
+plt.figure(1)
+plt.plot(s_rk, y_caso1, 'b')
+plt.xlabel('$s$')
+plt.ylabel('$y(s)$')#, fontsize=18)
+
+plt.figure(2)
+plt.plot(s_rk, y_caso2, 'g')
+plt.xlabel('$s$')
+plt.ylabel('$y(s)$')#, fontsize=18)
+
+plt.figure(3)
+plt.plot(y_caso1,v_caso1,'b')
+plt.xlabel('$y (s)$')
+plt.ylabel('$dy/ds$')
+
+plt.figure(4)
+plt.plot(y_caso2,v_caso2,'g')
+plt.xlabel('$y (s)$')
+plt.ylabel('$dy/ds$')
+
+plt.show()
+plt.draw()
