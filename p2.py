619c6c18037e1587aafa5192fe233f8317d7be17
Maria Constanza Flores <tatiflores.4@gmail.com>
2015-10-08 17:52:22 -0300

    Se agregan comentarios iniciales

--- p2.py
+++ p2.py
@@ -12,3 +12,10 @@ def funcion(t, p):
     dy = p[0] * (28 - p[2]) - p[1]
     dz = p[0] * p[1] - p[2]*(8./3)
     return [dx,dy,dz]
+
+#Condiciones iniciales
+t0 = 0.0001 #Cambiar por 0.0001 si no funciona
+x0 = 1
+y0 = 1
+z0 = 1
+p0 = [x0, y0, z0]
